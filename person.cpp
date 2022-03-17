25de80d051e3cbd38344839ef452d102b44ebd9a
henhes <hennihes@stud.ntnu.no>
2022-03-17 14:15:38 +0100

    fixed

--- person.cpp
+++ person.cpp
@@ -1,7 +1,3 @@
-//
-// Created by Henning Hestnes on 17/03/2022.
-//
-
 #include "person.h"
 
 Person::Person(const std::string &name) {
--- person.h
+++ person.h
@@ -1,6 +1,3 @@
-//
-// Created by Henning Hestnes on 17/03/2022.
-//
 #include <string>
 #ifndef UNTITLED2_PERSON_H
 #define UNTITLED2_PERSON_H
@@ -8,15 +5,12 @@
 
 class Person {
 public:
-Person(const std::string& name);
+    Person(const std::string& name);
 
-std::string getName()const;
+    std::string getName() const;
 
 private:
-std::string name_;
-
-
+    std::string name_;
 };
-
 
 #endif //UNTITLED2_PERSON_H
